98cd4f1a2979e085ef1756d389aec0c7c682be27
bipproduction <bip.production.js@gmail.com>
2025-10-23 16:28:19 +0800

    tambahannya

--- src/server/routes/wa_hook_route.ts
+++ src/server/routes/wa_hook_route.ts
@@ -49,7 +49,7 @@ async function flowAi({
         logger.info("[POST] flow found");
 
         await client.markMessageAsRead(message.id);
-        await client.sendTypingIndicatorWithDuration(message.from, 5000);
+        await client.sendTypingIndicator("+6289697338821");
 
         const { flowUrl, flowToken } = flow;
 
