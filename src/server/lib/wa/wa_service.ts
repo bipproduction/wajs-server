351383e36e5031999c21cb4dad9d3e2cc2e0a935
bipproduction <bip.production.js@gmail.com>
2026-02-21 12:01:26 +0800

    tambahan

--- src/server/lib/wa/wa_service.ts
+++ src/server/lib/wa/wa_service.ts
@@ -118,6 +118,7 @@ async function startClient() {
     state.isStarting = true;
 
     await destroyClient();
+    
 
     log('🚀 Memulai WhatsApp client...');
     handleHook({ eventType: "start" });
